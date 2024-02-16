7c53e1f701e68c50f52ac8c90ab9fefe3c27db22
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-02-16 13:09:26 +0530

    added route for submitQuestionnaire

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 
+app.post("/submitQuestionnaire",(req,res)=>{
+    var data=req.body
+    res.json({"status":"success","data":data})
+}
+
+)
+
 
 app.listen(4000,()=>{
     console.log("server running on port 4000....")
