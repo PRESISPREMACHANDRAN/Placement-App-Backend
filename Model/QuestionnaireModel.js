6018f529a7fa4d8f760f5d54c546e6fcecfb6d25
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-02-16 15:54:03 +0530

    connected to mongoBD and submitQuestionnaire completed

---
+++ Model/QuestionnaireModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const questionnaireSchema = mongoose.Schema({
+  email: { type: String, required: true },
+  name: { type: String, required: true },
+  studentID: { type: String, required: true },
+  stream: { type: String, required: true },
+  photo: { type: String, required: true },
+
+  registeredInPlacementPortal: { type: Boolean, required: true },
+  placementID: { type: String },
+
+  attendedInterviews: { type: Boolean },
+
+  areaOfInterest: { type: [String], required: true },
+
+  receivedJobOffers: { type: Boolean, required: true },
+  jobOfferDocument: { type: String }
+});
+var questionnaireModel = mongoose.model("questionnaire", questionnaireSchema);
+
+module.exports={questionnaireModel}
